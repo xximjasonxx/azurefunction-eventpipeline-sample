efda9be502dc3c5d4bd0422f3d487b4c5ff54c8b
Jason Farrell <jason.farrell@newsignature.com>
2020-04-26 00:02:49 -0400

    adjustments to create aggregate document

--- CreateAggregateDocument.cs
+++ CreateAggregateDocument.cs
@@ -13,20 +13,19 @@ namespace Farrellsoft.Examples
         [FunctionName("CreateAggregateDocument")]
         public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
-            [CosmosDB(databaseName: "namesdata", collectionName: "firstletters",
+            [CosmosDB(databaseName: "namesdata", collectionName: "firstletterstrend",
                 ConnectionStringSetting = "CosmosDbConnection")]out dynamic document,
             ILogger log)
         {
             //log.LogInformation("CreateAggregateDocument starting");
 
             var eventDataContents = req.ReadAsStringAsync().GetAwaiter().GetResult();
-            var countsArray = JArray.Parse(eventDataContents);
-
+            
             // create the collated event data into our document db
             document = new {
                 id = Guid.NewGuid(),
                 timestamp = DateTime.UtcNow,
-                eventData = countsArray
+                eventData = JArray.Parse(eventDataContents)
             };
 
             //log.LogTrace($"Registered document with id {document.id} for creation");
