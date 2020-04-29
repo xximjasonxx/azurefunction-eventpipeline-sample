6540c07445b393aff5a181c359e26e03f377b43f
Jason Farrell <jason.farrell@newsignature.com>
2020-04-28 20:24:36 -0400

    Changed over to using Azure Service Bus for queing

--- DequeueGeneratedNames.cs
+++ DequeueGeneratedNames.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Farrellsoft.Examples
 {
@@ -13,20 +15,41 @@ namespace Farrellsoft.Examples
         [FunctionName("DequeueGeneratedNames")]
         [return: Blob("raw-names/{id}.txt", FileAccess.Write, Connection = "AzureWebJobsStorage")]
         public static string Run(
-            [QueueTrigger("names-queue", Connection = "AzureWebJobsStorage")]string myQueueItem,
+            //[QueueTrigger("names-queue", Connection = "AzureWebJobsStorage")]string myQueueItem,
+            [ServiceBusTrigger("newnames-queue", Connection = "ServiceBusConnection")]NamesGenerationRecord msg,
             [EventHub("names", Connection = "EventHubSendConnection")]IAsyncCollector<string> outputEvents,
             ILogger log)
         {
             //log.LogTrace($"Dequeued {myQueueItem} from Queue");
 
-            ((JArray)JObject.Parse(myQueueItem)["data"]).ToList()
-                .ForEach((obj) =>
-                {
-                    //log.LogTrace($"Hello {obj["name"].Value<string>()}");
-                    outputEvents.AddAsync(obj.ToString());
-                });
+            foreach (var nameRecord in msg.GeneratedNames)
+            {
+                //log.LogTrace($"Hello {obj["name"].Value<string>()}");
+                outputEvents.AddAsync(nameRecord.ToString());
+            }
             
-            return myQueueItem;
+            return msg.ToString();
+        }
+    }
+
+    public class NamesGenerationRecord
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("data")]
+        public IList<JObject> GeneratedNames {get; set; }
+
+        public override string ToString()
+        {
+            var data = new JObject(
+                new JProperty("id", Id),
+                new JProperty("data", GeneratedNames.Select(x => new JObject(
+                    new JProperty("name", x)
+                )))
+            );
+
+            return data.ToString();
         }
     }
 }
--- GenerateNamesFunctions.cs
+++ GenerateNamesFunctions.cs
@@ -13,7 +13,8 @@ namespace Farrellsoft.Examples
     public static class GenerateNamesFunctions
     {
         [FunctionName("GenerateNamesTimerFunction")]
-        [return: Queue("names-queue", Connection = "AzureWebJobsStorage")]
+        //[return: Queue("names-queue", Connection = "AzureWebJobsStorage")]
+        [return: ServiceBus("newnames-queue", Connection = "ServiceBusQueueConnection")]
         public static async Task<string> RunTrigger(
             [TimerTrigger("*/3 * * * * *")]TimerInfo myTimer,
             ILogger log
